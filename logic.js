885e9817dd2a56f9439f520fac01b122c1e9103e
Ryan Helgerson <ryan.helgerson@gmail.com>
2022-01-03 21:24:23 -0600

    well its something.

--- logic.js
+++ logic.js
@@ -2,12 +2,17 @@ let city = "";
 const cityInput = document.querySelector("#city-input");
 const inputBtn = document.querySelector("#search-btn");
 const APIKey = "11f79b3d45f42e998837721c4179ed02";
-const citySearchURL = `api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${APIKey}`
+const citySearchURL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
 
-function getCity(e){
+function getForecast(e){
     e.preventDefault();
     city = cityInput.value;
-    console.log(city); 
+    console.log(city);
+    fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+    
 }
 
-inputBtn.addEventListener("click", getCity);
+inputBtn.addEventListener("click", getForecast);
