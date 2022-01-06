307aebef8f7639a024b187f59a79d19b143d3a96
Ryan Helgerson <ryan.helgerson@gmail.com>
2022-01-05 20:34:55 -0600

    saving searches to local storage

--- logic.js
+++ logic.js
@@ -2,22 +2,34 @@ const cityInput = document.querySelector("#city-input");
 const searchForm = document.querySelector("#search-form");
 const APIKey = "b4c54130ab13df94f32df953f198e331";
 
-function getCoords(input){
-    console.log(input);
-    let city = input;
-    let cityCoordURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
-    fetch(cityCoordURL)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
 
+
+function renderForecast(info) {
+    console.log(info);
+    let temperature = Math.round((info.main.temp - 273.5) * 1.8 + 32);
+    let high = Math.round((info.main.temp_max - 273.5) * 1.8 + 32);
+    let low = Math.round((info.main.temp_min - 273.5) * 1.8 + 32);
+    console.log(low);
+}
+
+function saveCity(input){
+    var history = JSON.parse(localStorage.getItem("search-history"));
+    if (!history) {
+        history = [];
+        history.push(input);
+        localStorage.setItem("search-history", input);
+    }
 }
 
 function getForecast(e){
     e.preventDefault();
-    city = cityInput.value.trim();
-    console.log(city);
-    getCoords(city);
+    let city = cityInput.value.trim();
+    let cityCoordURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`
+    saveCity(city);
+    fetch(cityCoordURL)
+        .then(response => response.json())
+        .then(data => renderForecast(data))
+        .catch(error => console.log(error));
     
 }
 
